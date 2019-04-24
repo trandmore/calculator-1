c8a541be3cd8b0c3bf6f25ba36e0fd80c9fbd898
juht <juht@kmu.ac.kr>
2019-04-24 16:29:26 +0900

    Add javaDoc to CalculatorApplicationTests.java

--- src/test/java/com/juht/calculator/CalculatorApplicationTests.java
+++ src/test/java/com/juht/calculator/CalculatorApplicationTests.java
@@ -5,8 +5,16 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/** 
+  * Used for Calculator Test
+  * @author Hongtaek Ju
+  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
+/** 
+  * Used for Calculator Test
+  * @author Hongtaek Ju
+  */
 public class CalculatorApplicationTests {
 
 	@Test
