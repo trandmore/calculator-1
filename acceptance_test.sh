db95a08283f3fcb56f2ed1130330139afb9327df
juht <juht@kmu.ac.kr>
2019-04-24 18:19:07 +0900

    modify acceptance_test.sh

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-test $(curl --silent localhost:8090/sum?a=1\&b=2) -eq 4 || \
-test $(curl --silent $(docker network inspect -f '{{range .IPAM.Config}}{{.Gateway}}{{end}}' bridge):8090/sum?a=1\&b=2) -eq 5
+test $(curl --silent localhost:8090/sum?a=1\&b=2) -eq 3 || \
+test $(curl --silent $(docker network inspect -f '{{range .IPAM.Config}}{{.Gateway}}{{end}}' bridge):8090/sum?a=1\&b=2) -eq 3
