09ad6e425cf1d5eb3165cb4a1b3e8cf3d5685368
JUHT <juht@kmu.ac.kr>
2019-04-30 14:57:32 +0900

    change option --silent to acceptance_test.sh

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-test $(curl --silence localhost:8090/sum?a=1\&b=2) -eq 3 || \
-test $(curl --silence $(docker network inspect -f \
+test $(curl --silent localhost:8090/sum?a=1\&b=2) -eq 3 || \
+test $(curl --silent $(docker network inspect -f \
      '{{range .IPAM.Config}}{{.Gateway}}{{end}}' bridge):8090/sum?a=1\&b=2) –eq 3
